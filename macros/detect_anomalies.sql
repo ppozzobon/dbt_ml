b8fc6406c16f60ffcff9f5e373611952b9ffab79
ppozzobon <59428040+ppozzobon@users.noreply.github.com>
2024-12-03 12:54:29 -0300

    fix setting name detect_anomalies.sql

--- macros/detect_anomalies.sql
+++ macros/detect_anomalies.sql
@@ -1,7 +1,7 @@
 {% macro detect_anomalies(relation, source, threshold = 0.95) %}
     ml.detect_anomalies(
         model {{ relation }},
-        struct({{ threshold }} as anomaly_prob_threshold),
+        struct({{ threshold }} as contamination),
         (select * from {{ source }})
     )
 {% endmacro %}
